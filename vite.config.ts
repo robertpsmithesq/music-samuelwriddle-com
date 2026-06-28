d055ce348f799735598cf47760df698b13d0a4eb
Sam Riddle <sriddle@quanthub.com>
2026-06-28 02:37:37 -0500

    Add start command

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
@@ -13,10 +13,10 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter(),
+			adapter: adapter({
+				pages: 'dist',
+				assets: 'dist'
+			}),
 			preprocess: [
 				mdsvex({
 					extensions: ['.svx', '.md']
